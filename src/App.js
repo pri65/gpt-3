786c877777eea20d7109ec699a1c500384801023
pri65 <priyankasoniii65@gmail.com>
2022-11-13 18:07:32 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers';
+import { Cta, Brand, Navbar } from './components';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
     </div>
   )
 }
