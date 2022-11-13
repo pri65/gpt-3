01f42f10efd063cf3241f086eda27a99fcd412a8
pri65 <priyankasoniii65@gmail.com>
2022-11-13 18:10:28 +0530

    App css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers';
 import { Cta, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
