ab06456d06731b57d414caae2167c9c46d3e7518
pri65 <priyankasoniii65@gmail.com>
2022-11-13 13:07:44 +0530

    Bigger component structured

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import Article from './components/article/Article';
-import Brand from './components/brand/Brand';
-import Cta from './components/cta/Cta';
-import Feature from './components/feature/Feature';
-import Navbar from './components/navbar/Navbar';
+
 
 const App = () => {
   return (
     <div>
         <h1>GPT-3</h1>
-        <Article />
-        <Brand />
-        <Cta />
-        <Feature />
-        <Navbar />
     </div>
   )
 }
