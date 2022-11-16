3dccb1102aeb211540524b59b61a31713bb3c777
pri65 <priyankasoniii65@gmail.com>
2022-11-16 14:15:02 +0530

    Added links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,6 +10,13 @@ const Navbar = () => {
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt="logo" />
         </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#home'>What is GPT?</a></p>
+          <p><a href='#home'>Open AI</a></p>
+          <p><a href='#home'>Case Studies</a></p>
+          <p><a href='#home'>Library</a></p>
+        </div>
       </div>
       </div>
   )
