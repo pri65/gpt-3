4f2b6496690d2b605f306e865e15399f5e4a5281
pri65 <priyankasoniii65@gmail.com>
2022-11-16 14:16:46 +0530

    Naming links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
         </div>
         <div className='gpt3__navbar-links_container'>
           <p><a href='#home'>Home</a></p>
-          <p><a href='#home'>What is GPT?</a></p>
-          <p><a href='#home'>Open AI</a></p>
-          <p><a href='#home'>Case Studies</a></p>
-          <p><a href='#home'>Library</a></p>
+          <p><a href='#wgpt3'>What is GPT?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
         </div>
       </div>
       </div>
